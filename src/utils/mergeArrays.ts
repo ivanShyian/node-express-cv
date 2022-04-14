d77b7ab7385ccdb648440bebbaac1193873c5194
vanjkes <shyian.ivan@yahoo.com>
2022-04-15 00:53:44 +0300

    Refactored mergeArray function

--- src/utils/mergeArrays.ts
+++ src/utils/mergeArrays.ts
@@ -1,21 +1,16 @@
 import {Types} from 'mongoose'
 
 export default function(databaseArray: any[], userArray: any[], lang: string) {
-  const result = []
+  const result = [...databaseArray]
 
-  loop1:
-  for(let i = 0; i < userArray.length; i++) {
-    const userObj = userArray[i]
-    for(let j = 0; j < databaseArray.length; j++) {
-      const dbObj = databaseArray[j]
-      if (dbObj._id.toString() === userObj._id) {
-        result.push({ ...dbObj._doc, [lang]: userObj.value })
-        continue loop1
-      } else if (!userObj._id) {
-        result.push({ [lang]: userObj.value, _id: new Types.ObjectId() })
-        continue loop1
-      }
+  userArray.forEach((userObj: any) => {
+    const foundElementIndex = databaseArray.findIndex(el => el._id.toString() === userObj._id)
+    if (foundElementIndex >= 0) {
+      result[foundElementIndex] = {...result[foundElementIndex]._doc, [lang]: userObj.value}
+    } else {
+      result.push({ [lang]: userObj.value, _id: new Types.ObjectId() })
     }
-  }
+  })
+
   return result
 }
