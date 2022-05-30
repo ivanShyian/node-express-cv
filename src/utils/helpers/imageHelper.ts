222df7e235ecdd6eb07642ea2869c2356d780694
vanjkes <shyian.ivan@yahoo.com>
2022-05-30 22:33:14 +0300

    Fixed image helper for dev

--- src/utils/helpers/imageHelper.ts
+++ src/utils/helpers/imageHelper.ts
@@ -18,17 +18,20 @@ export const convertImageWithSharp = async(file: Express.Multer.File): Promise<{
   filePath: string
   fileBase64: string
 }> => {
+
   let filePath = file.path
     .replace('src\\', '')
     .replace('\\', '/')
     .replace(formatRegex, '.webp')
 
+  const sharpFilePath = path.join(__dirname, isProduction ? '../../../' : '../../../src', filePath)
+
   await sharp(path.join(__dirname, '../../../', file.path))
     .resize({fit: "inside", height: 500})
     .webp({quality: 100})
-    .toFile(path.join(__dirname, '../../../', filePath))
+    .toFile(sharpFilePath)
 
-  const base64 = await sharp(path.join(__dirname, isProduction ? '../../../' : '../../../src', filePath))
+  const base64 = await sharp(sharpFilePath)
     .resize({fit: "cover", height: 350, width: 350})
     .toBuffer()
 
